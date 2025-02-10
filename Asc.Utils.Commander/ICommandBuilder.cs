16b1aa47b92c8ea3da018f5d46a9c34f6cbc1a00
asuarezc <alberto.suarez.canovas@gmail.com>
2025-02-10 12:41:15 +0100

    Added public interfaces

---
+++ Asc.Utils.Commander/ICommandBuilder.cs
@@ -0,0 +1,36 @@
+﻿namespace Asc.Utils.Commander;
+
+public interface ICommandBuilderBase
+{
+    ICommandBuilder OnSpecificFailure<TException>(Action<TException> onFailure) where TException : Exception;
+
+    ICommandBuilder OnSpecificFailure<TException>(Func<TException, Task> onSuccess) where TException : Exception;
+
+    ICommandBuilder OnFailure(Action<Exception> onFailure);
+
+    ICommandBuilder OnFailure(Func<Exception, Task> onFailure);
+
+    ICommand Build();
+}
+
+public interface ICommandBuilder : ICommandBuilderBase
+{
+    ICommandBuilder Job(Action job);
+
+    ICommandBuilder Job(Func<Task> job);
+
+    ICommandBuilder OnSuccess(Action onSuccess);
+
+    ICommandBuilder OnSuccess(Func<Task> onSuccess);
+}
+
+public interface ICommandBuilder<TResult> : ICommandBuilderBase
+{
+    ICommandBuilder Job(Func<TResult> job);
+
+    ICommandBuilder Job(Func<Task<TResult>> job);
+
+    ICommandBuilder OnSuccess(Action<TResult> onSuccess);
+
+    ICommandBuilder OnSuccess(Func<TResult, Task> onSuccess);
+}
---
+++ Asc.Utils.Commander/Implementation/CommandBuilder.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asc.Utils.Commander.Implementation;
+
+internal class CommandBuilder : ICommandBuilder
+{
+    public ICommand Build()
+    {
+        throw new NotImplementedException();
+    }
+
+    public ICommandBuilder Job(Action job)
+    {
+        return this;
+    }
+
+    public ICommandBuilder Job(Func<Task> job)
+    {
+        return this;
+    }
+
+    public ICommandBuilder OnFailure(Action<Exception> onFailure)
+    {
+        return this;
+    }
+
+    public ICommandBuilder OnFailure(Func<Exception, Task> onFailure)
+    {
+        return this;
+    }
+
+    public ICommandBuilder OnSpecificFailure<TException>(Action<TException> onFailure) where TException : Exception
+    {
+        return this;
+    }
+
+    public ICommandBuilder OnSpecificFailure<TException>(Func<TException, Task> onSuccess) where TException : Exception
+    {
+        return this;
+    }
+
+    public ICommandBuilder OnSuccess(Action onSuccess)
+    {
+        return this;
+    }
+
+    public ICommandBuilder OnSuccess(Func<Task> onSuccess)
+    {
+        return this;
+    }
+}
